5e8e7b84e96e062a067ef055f95ef55c603c9db2
dmgorlesky1 <dmgorlesky1@agora.cs.wcu.edu>
2019-04-21 14:03:03 -0400

    Added test cases for Journey

--- tests_railway.js
+++ tests_railway.js
@@ -1,1 +1,7 @@
-hi
+/**
+ * TDD Style Testing
+ **/
+var assert = require('chai').assert;
+var railway = require('./railway.js');
+var fs = require('fs');
+/** A test suite for testing the railway object **/
